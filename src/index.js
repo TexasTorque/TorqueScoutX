a6c9de1838f5b01896a2c7d6f88450288ac9c084
Suhas44 <suhas8@outlook.com>
2023-01-09 11:57:06 -0600

    Fonts and Arcitecture

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import './index.css';
+import Home from './pages/Home';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+ReactDOM.render(
   <React.StrictMode>
+    <Route path="/" element={<Home />} />
+    <Route path="/home" element={<Home />} />
   </React.StrictMode>
 );
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
---
+++ src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import { useNavigate } from "react-router-dom";
+
+const Home = () => {
+  const navigate = useNavigate();
+  return (
+    <div className="home">
+      <div className="container mt-4">
+      </div>
+    </div>
+  );
+};
+
+export default Home;
