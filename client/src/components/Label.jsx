ab3fb75c8bb538f46d049cbe0957bb15953a5c1e
JoseB1717 <158538517+JoseB1717@users.noreply.github.com>
2025-03-28 19:45:36 -0500

    Even Smaller Label.jsx

--- client/src/components/Label.jsx
+++ client/src/components/Label.jsx
@@ -1,13 +1,22 @@
 const Label = ({ name }) => {
     const splitText = name.split(",,").map((line, index) => (
-        <div key={index} style={{ textAlign: "center", wordWrap: "break-word" }}>{line}</div>
+        <div key={index} style={{ textAlign: "center", wordBreak: "break-word" }}>{line}</div>
     ));
 
     return (
-        <div className="label" style={{ fontSize: "1.3rem", padding: "5px" }}>
-            <hr style={{ borderTop: "3px solid lightgray" }} />
+        <div 
+            className="label" 
+            style={{ 
+                fontSize: "1rem",
+                padding: "2px 2px",  
+                maxWidth: "fit-content",
+                display: "inline-block",
+                textAlign: "center"
+            }}
+        >
+            <hr style={{ borderTop: "1px solid lightgray", margin: "2px 0" }} />
             {splitText}
-            <hr style={{ borderTop: "3px solid lightgray" }} />
+            <hr style={{ borderTop: "1px solid lightgray", margin: "2px 0" }} />
         </div>
     );
 };
